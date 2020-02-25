cb932b55b9f022965bab47fcbeb1edeb512c6f21
WarrenMfg <warrenmfg@icloud.com>
2020-02-25 15:19:09 -0500

    changed MongoClient url; trying to connect to remote db

--- database/mongoDB/index.js
+++ database/mongoDB/index.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const client = new MongoClient('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient('mongodb://13.59.77.47:27017', { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect();
 
 module.exports = client;
